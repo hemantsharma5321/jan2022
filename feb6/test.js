cfd3c093d3425f26cb04b4fc0aa65825845596dd
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-02-07 00:38:47 +0530

    Update test.js

--- feb6/test.js
+++ feb6/test.js
@@ -1,62 +1,62 @@
 var all_posts=[
-   {
-       'title':"india",
-       'date':"02-06-2022",
-       'content':"indian is great country"
-   },
-   {
-    'title':"india",
-    'date':"02-06-2022",
-    'content':"indian is great country"
-},
-{
-    'title':"india",
-    'date':"02-06-2022",
-    'content':"indian is great country"
-},
-{
-    'title':"india",
-    'date':"02-06-2022",
-    'content':"indian is great country"
-}
+//    {
+//        'title':"",
+//        'date':"",
+//        'content':"indian is great country"
+//    }
 
 ];
 
-for(let i=0;i<all_posts.length;i++){
+function show(){
     var container=document.getElementById("container");
+    container.innerHTML="";
+for(var i=0;i<all_posts.length;i++){
     
-    var post=document.createElement("div");
-    post.classList.add("post");
-    var title=document.createElement("h3");
-    
-    var date=document.createElement("p");
-    var postbtns=document.createElement("div");
-    postbtns.classList.add("postbtns");
-    var delbtn=document.createElement("button");
-    var updatebtn=document.createElement("button");
-    var previewbtn=document.createElement("button");
-    delbtn.innerText="delete";
-    updatebtn.innerText="Update";
-    previewbtn.innerText="preview";
-    title.innerText=all_posts[i].title;
-    date.innerText=all_posts[i].date;
-    post.append(title,date);
-    postbtns.append(delbtn,updatebtn,previewbtn);
-    container.append(postbtns,post); 
-     
+    var posthtml=`<div class="post">
+<h3 id="title">${all_posts[i]['title']}</h3>
+<p id="date"><span>Published</span>${all_posts[i]['date']}</p> 
+<div class="postbtns">
+    <button id="delbtn" onclick="deletefn(${i})">Delete</button>
+    <button id="previewbtn" onclick="preview(${i})">Preview</button>
+    <button id="updatebtn" onclick="update">Edit</button>
+</div>
+</div>`;
+    container.innerHTML+=posthtml;
 }
-
-
+}
 function publish(){
     var title=document.getElementById("titleinput").value;
-    var d=new Date();
+    var d=new Date()
     var date=d.getDate();
     var content=document.getElementById("contentinput").value;
-
     all_posts.push({
-        'title':{title},
-        'date':date,
-        'content':content
-    })
-
+        'title':`${title}`,
+        'date':`${date}`,
+        'content':`${content}`
+    });
+    show();
 }
+function deletefn(index){
+    all_posts.splice(index,index+1);
+    show();
+}
+function showcreate(){
+    let item=document.getElementById("createblog");
+    let item2=document.getElementById("preview");
+    item2.classList.add('dnone');
+    item.classList.remove('dnone');
+}
+function preview(index){
+    var item=document.getElementById("createblog");
+    let item2=document.getElementById("preview");
+    item.classList.add('dnone');
+    item2.classList.remove('dnone');
+    item2.innerHTML=`<h3 id="prevtitle">${all_posts[index]['title']}</h3>
+    <span style="color:red" onclick="hideprev()">X</span>
+    <p id="prevcontent">${all_posts[index]['content']}</p>`
+}
+function hideprev(){
+    let item2=document.getElementById("preview");
+    item2.classList.add('dnone');
+}
+
